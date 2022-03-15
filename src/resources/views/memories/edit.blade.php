3f49812a98eaf5e7c4a8b83af192ec05e9b7d9ca
aonori0819 <noriko0727eh@gmail.com>
2022-03-15 12:53:41 +0900

    [feature]思い出投稿に画像アップロード機能を追加

--- src/resources/views/memories/edit.blade.php
+++ src/resources/views/memories/edit.blade.php
@@ -10,20 +10,38 @@
         <div class="card mt-3">
           <div class="card-body pt-3">
             <div class="card-text">
-              <form method="POST" action="{{ route('memories.update', ['memory' => $memory] ) }}">
+              <form method="POST" action="{{ route('memories.update', ['memory' => $memory] ) }}" enctype="multipart/form-data" >
                 @method('PATCH')
                 @csrf
                 <div class="form-group">
+                    {{-- 本文 --}}
                     <textarea name="body" required class="form-control" rows="16" placeholder="ひとこと・できごと・おもいを入力">{{ $memory->body ?? old('body') }}
                     </textarea>
+                    {{-- 画像アップロード --}}
+                    @if ($memory->image_path)
+                        <div class="image-upload">
+                            <img src="{{ asset('storage/upload/' . $memory->image_path ) }}" width="150" alt="思い出の画像">
+                        </div>
+                    @endif
+                    <span class="image-picker">
+                        <label>写真・画像を変更</label>
+                        <input type="file" name="image_path" accept="image/png,image/jpeg,image/gif,image/svg" >
+                    </span>
+                    <div class="delete-image">
+                        <label>写真・画像を削除</label>
+                        <input type="checkbox" name="delete_image" value="true">
+                    </div>
                     {{-- お子さまを登録済の場合、お子さまを選ぶボタン（任意選択）を表示 --}}
                     @if (isset($child_list))
+                    <div class="select-child">
+                        <p>どちらのお子さまの記録ですか？</p>
                         @foreach($child_list as $id => $name)
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="checkbox" name="children[]" value="{{ $id }}" {{ $memory->children->contains($id) ? 'checked' : '' }}>
-                                <label class="form-check-label" for="inlineRadio1">{{ $name }}</label>
+                                <label class="form-check-label" >{{ $name }}</label>
                             </div>
                         @endforeach
+                    `</div>
                     @endif
                 </div>
                 <button type="submit" class="btn blue-gradient btn-block">更新する</button>
