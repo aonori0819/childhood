54033b29444144ce7f44637bd0d625966e5895e3
aono <98136753+aonori0819@users.noreply.github.com>
2022-03-25 16:26:26 +0900

    Update CommentControllerTest.php

--- src/tests/Feature/CommentControllerTest.php
+++ src/tests/Feature/CommentControllerTest.php
@@ -47,12 +47,10 @@ class CommentControllerTest extends TestCase
     public function store_familyid設定後にコメントの新規登録に成功する() //同じfamily_idに紐づく思い出に対してコメント
     {
         $this->actingAs($this->user);
-        $this->user->user_detail = UserDetail::factory()->create(['user_id' => $this->user->id]);
+        $this->user->user_detail = UserDetail::factory()->for($this->user)->create();
         $family = Family::factory()->create();
         $this->user->user_detail->family_id = $family->id;
-        $memory = Memory::factory()->create([
-            'family_id' => $family->id,
-        ]);
+        $memory = Memory::factory()->for($family)->create();
 
         $data = ['body' => '家族からのコメント新規登録テスト'];
         $response = $this->post(route('comments.store', ['memory_id' => $memory->id]), $data);
